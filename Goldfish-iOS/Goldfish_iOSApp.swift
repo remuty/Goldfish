1496c8b02f0a3b87f81e54fe84b446088e502319
remuty <remuty.work@gmail.com>
2021-09-14 17:56:12 +0900

    add Goldfish-iOS

---
+++ Goldfish-iOS/Goldfish_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goldfish_iOSApp.swift
+//  Goldfish-iOS
+//
+//  Created by remuty on 2021/09/14.
+//
+
+import SwiftUI
+
+@main
+struct Goldfish_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
