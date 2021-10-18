3b20c94e5b42a26d6cda18791b4b95e53fb2c981
remuty <remuty.work@gmail.com>
2021-10-19 05:02:03 +0900

    add エサやりの時間を自動設定

--- Goldfish-iOS/Model/Time.swift
+++ Goldfish-iOS/Model/Time.swift
@@ -10,11 +10,22 @@ import Foundation
 class Time: ObservableObject {
     @Published var nextTime : String = ""
     func calculateTime() {
+        let feedingTime = 8
         let calendar = Calendar(identifier: .gregorian)
         let now = Date()
-        let next = calendar.date(from: DateComponents(year: 2021, month: 10, day: 19, hour: 12))
+        let nowDC = calendar.dateComponents([.year, .month, .day,.hour], from: now)
+        var nextDay = Date()
+        var nextDayDC = DateComponents()
+        var next = Date()
+        if nowDC.hour! >= feedingTime{
+            nextDay = calendar.date(byAdding: .day, value: 1, to: now)!
+            nextDayDC = calendar.dateComponents([.year, .month, .day], from: nextDay)
+            next = calendar.date(from: DateComponents(year: nextDayDC.year, month: nextDayDC.month, day: nextDayDC.day, hour: feedingTime))!
+        }else{
+            next = calendar.date(from: DateComponents(year: nowDC.year, month: nowDC.month, day: nowDC.day, hour: feedingTime))!
+        }
         //次の時間までの差
-        let diff = calendar.dateComponents([.second], from: now, to: next!)
+        let diff = calendar.dateComponents([.second], from: now, to: next)
         
         // 書式を設定する
         let formatter = DateComponentsFormatter()
@@ -24,5 +35,4 @@ class Time: ObservableObject {
         formatter.allowedUnits = [.hour, .minute, .second]
         self.nextTime = formatter.string(from: diff)!
     }
-    
 }
